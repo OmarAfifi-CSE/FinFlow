9766c8b0634c7d893015633030e396081ded924f
Omar Afifi <omarafifi.cse@gmail.com>
2025-06-02 10:13:33 +0300

    Add Category Screen

---
+++ lib/models/expense_category.dart
@@ -0,0 +1,26 @@
+class ExpenseCategory {
+  final String id;
+  final String name;
+  final bool? isDefault;
+
+  ExpenseCategory({
+    required this.id,
+    required this.name,
+     this.isDefault,
+  });
+
+  factory ExpenseCategory.fromJson(Map<String, dynamic> json) {
+    return ExpenseCategory(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+
+}
