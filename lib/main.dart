82ca64757f611140fc76cd227f6ed67f181c3bcc
Omar Afifi <omarafifi.cse@gmail.com>
2025-06-02 10:11:19 +0300

    Add Home Screen

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/expense_provider.dart';
+import 'screens/category_management_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/tag_management_screen.dart';
+import 'screens/onboarding_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initLocalStorage();
+
+  runApp(MyApp(localStorage: localStorage));
+}
+
+class MyApp extends StatelessWidget {
+  final LocalStorage localStorage;
+
+  const MyApp({Key? key, required this.localStorage}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ExpenseProvider(localStorage)),
+      ],
+      child: MaterialApp(
+        title: 'Expense Tracker',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Inter'),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => OnboardingScreen(),
+          '/home_screen': (context) => HomeScreen(),
+          '/manage_categories': (context) => CategoryManagementScreen(),
+          '/manage_tags': (context) => TagManagementScreen(),
+        },
+      ),
+    );
+  }
+}
