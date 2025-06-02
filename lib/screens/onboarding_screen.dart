981e3f0ab440c3d15f37504f5307d73e9f327c74
Omar Afifi <omarafifi.cse@gmail.com>
2025-06-02 10:10:10 +0300

    Add Onboarding Screen

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,73 @@
+import 'package:expense_manager/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffE5E5E5),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage('assets/images/Onboarding.png'),
+              width:
+                  MediaQuery.of(context).size.width < 500
+                      ? MediaQuery.of(context).size.width
+                      : 300,
+              fit: BoxFit.fitWidth,
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+
+            Text(
+              "Spend Smarter\nSave More",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Color(0xff438883),
+              ),
+            ),
+
+            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal:
+                    MediaQuery.of(context).size.width < 500
+                        ? MediaQuery.of(context).size.width * .1
+                        : MediaQuery.of(context).size.width * .2,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed:
+                      () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff438883),
+                    foregroundColor: Colors.white,
+                    elevation: 5,
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    textStyle: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: const Text("Get Started"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
