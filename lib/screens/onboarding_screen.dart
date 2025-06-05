9d0024192eab9c72a7044e6279c2b326a1c6d4dd
Omar Afifi <omarafifi.cse@gmail.com>
2025-06-06 01:45:38 +0300

    Adjust Onboarding screen
    - for different screens size

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -6,68 +6,66 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffE5E5E5),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image(
-              image: AssetImage('assets/images/Onboarding.png'),
-              width:
-                  MediaQuery.of(context).size.width < 500
-                      ? MediaQuery.of(context).size.width
-                      : 300,
-              fit: BoxFit.fitWidth,
-            ),
+    final content = Column(
+      children: [
+        Image(
+          image: AssetImage('assets/images/Onboarding.png'),
+          width: MediaQuery.of(context).size.width < 500
+              ? MediaQuery.of(context).size.width
+              : 300,
+          fit: BoxFit.fitWidth,
+        ),
 
-            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
 
-            Text(
-              "Spend Smarter\nSave More",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-                color: Color(0xff438883),
-              ),
-            ),
+        Text(
+          "Spend Smarter\nSave More",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40,
+            color: Color(0xff438883),
+          ),
+        ),
 
-            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.05),
 
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal:
-                    MediaQuery.of(context).size.width < 500
-                        ? MediaQuery.of(context).size.width * .1
-                        : MediaQuery.of(context).size.width * .2,
-              ),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed:
-                      () => Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => HomeScreen()),
-                      ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff438883),
-                    foregroundColor: Colors.white,
-                    elevation: 5,
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    textStyle: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: const Text("Get Started"),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width < 500
+                ? MediaQuery.of(context).size.width * .1
+                : MediaQuery.of(context).size.width * .2,
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () => Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (context) => HomeScreen())),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xff438883),
+                foregroundColor: Colors.white,
+                elevation: 5,
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                textStyle: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
                 ),
               ),
+              child: const Text("Get Started"),
             ),
-          ],
+          ),
         ),
-      ),
+      ],
+    );
+    return Scaffold(
+      backgroundColor: Color(0xffE5E5E5),
+      body: MediaQuery.of(context).size.width < 500
+          ? SingleChildScrollView(child: content)
+          : Center(child: SingleChildScrollView(child: content)),
     );
   }
 }
