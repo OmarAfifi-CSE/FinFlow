7bb1f710a5f3294e3cee60b94220db525db6a067
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-22 17:38:56 +0300

    create app text styles

---
+++ lib/styling/app_text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+import 'app_fonts.dart';
+
+class AppTextStyles {
+  static TextStyle primaryHeadlineStyle = TextStyle(
+    color: AppColors.primaryColor,
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle subtitlesStyle = TextStyle(
+    color: AppColors.secondaryColor,
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle whiteTextStyle = TextStyle(
+    color: AppColors.whiteColor,
+    fontFamily: AppFonts.mainFontName,
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+}
