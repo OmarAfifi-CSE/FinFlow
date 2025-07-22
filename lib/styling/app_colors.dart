9051b6db4d97c2c7a2dd8d979c66f69d409c045c
Omar Afifi <omarafifi.cse@gmail.com>
2025-07-22 17:38:32 +0300

    create app colors

---
+++ lib/styling/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor = Colors.teal;
+  static const Color secondaryColor = Color(0xFF828282);
+  static const Color blackColor = Color(0xff1F2C37);
+  static const Color whiteColor = Color(0xffFFFFFF);
+}
